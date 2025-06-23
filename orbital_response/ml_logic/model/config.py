e8d13553b8cec57cc0a2a3e1060e328a8f4080ee
ChristianMirse <cmirosegui@gmail.com>
2025-06-23 15:13:55 +0200

    config file for training

---
+++ orbital_response/ml_logic/model/config.py
@@ -0,0 +1,11 @@
+LEARNING_RATE = 1e-4
+BATCH_SIZE = 5
+num_workers = 12  # Adjust depending on cpu
+
+EPOCHS_1 = 1000
+PATIENCE_1 = 300
+model_1_v = "Gaza_1st_BestModel_70.pth"
+
+EPOCHS_2 = 300
+PATIENCE_2 = 100
+model_2_v = "Gaza_2nd_BestModel_70.pth"
